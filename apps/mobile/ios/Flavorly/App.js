6f34ee74aeae85e42c8d7c5f89ee65a80c698067
Trey <treyoung12@yahoo.com>
2024-12-28 17:44:29 -0500

    adding the font family needed from the wireframe

--- apps/mobile/ios/Flavorly/App.js
+++ apps/mobile/ios/Flavorly/App.js
@@ -1,15 +1,40 @@
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
+import { StyleSheet, Text, View } from "react-native";
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, error] = useFonts({
+    "sofia-sans": require("./assets/fonts/SofiaSans-Regular.ttf"),
+    "sofia-sans-bold": require("./assets/fonts/SofiaSans-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app.js to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={{fontFamily: "sofia-sans"}}>This text is using 'Sofia Sans'!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F0ECD9",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
