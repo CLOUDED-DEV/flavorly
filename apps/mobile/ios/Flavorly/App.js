1d38bd88fb0ceb6c5dd6c221040235ce2b054df3
Trey <treyoung12@yahoo.com>
2024-12-31 15:30:20 -0500

    added splashscreen functionality, title with different colors on waitlist homepage, and added logo

--- apps/mobile/ios/Flavorly/App.js
+++ apps/mobile/ios/Flavorly/App.js
@@ -1,42 +1,67 @@
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { useEffect } from "react";
-import { StatusBar } from "expo-status-bar";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
+import { Asset } from "expo-asset";
 
 import WaitlistJoinScreen from "./screens/WaitlistJoinScreen";
 
+// prevent the splash screen from hiding automatically
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [fontsLoaded, error] = useFonts({
-    "sofia-sans": require("./assets/fonts/SofiaSans-Regular.ttf"),
-    "sofia-sans-bold": require("./assets/fonts/SofiaSans-Bold.ttf"),
-  });
-  
-// making sure the fonts load
   useEffect(() => {
-    if (fontsLoaded || error) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, error]);
+    const prepare = async () => {
+      try {
+        //preload assets
+        await Asset.loadAsync([
+          require("./assets/flavorly_logo.png"),
+          require("./assets/fonts/SofiaSans-Black.ttf"),
+          require("./assets/fonts/BlackHanSans-Regular.ttf"),
+        ]);
+        await new Promise((resolve) => setTimeout(resolve, 2000))
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // hide splash
+        await SplashScreen.hideAsync();
+      }
+    };
 
-  if (!fontsLoaded && !error) {
-    return null;
+    prepare();
+  }, []);
+  const [fontsLoaded, error] = useFonts({
+    "sofiasans-black": require("./assets/fonts/SofiaSans-Black.ttf"),
+    "BlackHanSans-Regular": require("./assets/fonts/BlackHanSans-Regular.ttf"),
+  });
+
+  // making sure the fonts load
+  if (!fontsLoaded || error) {
+    return console.log(error);
   }
 
-  let screen = <WaitlistJoinScreen/>
+  let screen = <WaitlistJoinScreen />;
 
-  return (
-    <SafeAreaView style={styles.container}>{screen}</SafeAreaView>
-  );
+  return <SafeAreaView style={styles.rootContainer}>{screen}</SafeAreaView>;
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootContainer: {
     flex: 1,
     backgroundColor: "#F0ECD9",
+  },
+  logoWrapper: {
+    borderWidth: "2",
     alignItems: "center",
-    justifyContent: "center",
+    padding: 20,
+  },
+  logo: {
+    height: 125,
+    width: 150,
+    marginTop: 40,
+    borderWidth: "2",
+    borderColor: "red",
+    resizeMode: "cover",
+    position: "absolute",
   },
 });
